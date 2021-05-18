eb961943679115a34b693fa09067248857cd4c82
Marty Ballard <marty331@gmail.com>
2021-05-18 12:40:40 -0500

    Make docs for existing builds

--- pre_commit_sphinx/build_docs.py
+++ pre_commit_sphinx/build_docs.py
@@ -19,7 +19,7 @@ def build(cache_dir: str, html_dir: str, src_dir: str):
     """
 
     # Run Sphinx to build the documentation
-    ret = os.system(f'sphinx-build -b html -d {cache_dir} {src_dir} {html_dir}')
+    ret = os.system(f'make html {src_dir} {html_dir}')
 
     # It's very weird that pre-commit marks this as 'PASSED' if I return an error code 512...! Workaround:
     return 0 if ret == 0 else 1
